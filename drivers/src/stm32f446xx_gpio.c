604aed1fc97fd69cccb2f75fd8fcd9e97e45e7bb
Jimmy Bates <jbates.productions@gmail.com>
2024-10-19 21:11:46 -0700

    Comments put in to fill out with code

--- drivers/src/stm32f446xx_gpio.c
+++ drivers/src/stm32f446xx_gpio.c
@@ -29,5 +29,38 @@ void GPIO_peri_clock_control(GPIO_TypeDef *p_GPIO_x, uint8_t en_state) {
 /*
  * Init and de-init of GPIO
  */
-void GPIO_init(GPIO_Handle_t *p_GPIO_handle);
+void GPIO_init(GPIO_Handle_t *p_GPIO_handle) {
+  if (p_GPIO_handle == NULL) return;
+
+  // Create pointers to the GPIO port and the pin configuration for easier
+  // access/readability
+  GPIO_TypeDef *gpiox = p_GPIO_handle->p_GPIO_x;
+  const GPIO_PinConfig_t *cfg = &(p_GPIO_handle->GPIO_pin_config);
+
+  // For easy bit-shifting, dshift is 2*pin number, whereas sshift is just
+  // pin_number
+
+  // Set mode
+  if (cfg->GPIO_pin_mode <= GPIO_MODE_ANALOG) {
+    // Esssentially, digital out, analog in, etc. Everything up and until interrupts
+  } else {
+    // Enable SysClk
+
+    /********* INTERRUPT LOGIC STARTS HERE **********/
+    // Turn on correct EXTI
+
+    // Configure correct edge
+
+    // Unmask bit in EXTI
+
+    // Make pin input
+  }
+  // Set output speed - clear bits to 00 and then set
+
+  // Set output type - clear bits to 0 first and then set
+
+  // Set pullup/pulldown resistor - clear bits to 00 and then set
+
+  // Configure alt functionality - clear bits to 0000 and then set
+}
 void GPIO_deinit(GPIO_TypeDef *p_GPIO_x);

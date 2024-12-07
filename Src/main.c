7236d191c4116c1ec0c8d846a0ade4db57e9690b
Jimmy Bates <jbates.productions@gmail.com>
2024-12-07 04:33:35 -0800

    Fixed instructions according to the stm32 advanced manual
    Added spaces (squash test)
    
    Git squash test2

--- Src/main.c
+++ Src/main.c
@@ -212,25 +212,26 @@ void spi_setup_test() {
 
   // 2.Write to the SPI_CR1 register:
 
-  // 3. Note:  a)Configure the serial clock baud rate using the BR[2:0] bits (Note: 3).
+  // a) Configure the serial clock baud rate using the BR[2:0] bits (Note: 3).
 
-  // b)Configure the CPOL and CPHA bits combination to define one of the fou  relationships between the data transfer and the serial clock. (Note: 2 - except the  case when CRC is enabled at TI mode).
+  // b) Configure the CPOL and CPHA bits combination to define one of the fou  relationships between the data transfer and the serial clock. (Note: 2 - except the  case when CRC is enabled at TI mode).
 
-  // c)Select simplex or half-duplex mode by configuring RXONLY or BIDIMODE and BIDIOE (RXONLY and BIDIMODE can't be set at the same time).
+  // c) Select simplex or half-duplex mode by configuring RXONLY or BIDIMODE and BIDIOE (RXONLY and BIDIMODE can't be set at the same time).
 
-  // d)Configure the LSBFIRST bit to define the frame format (Note: 2).
+  // d) Configure the LSBFIRST bit to define the frame format (Note: 2).
 
-  // e)Configure the CRCEN and CRCEN bits if CRC is needed (while SCK clock signal is at idle state
+  // e) Configure the CRCEN and CRCEN bits if CRC is needed (while SCK clock signal is at idle state
 
-  // f)Configure SSM and SSI (Note: 2).
+  // f) Configure SSM and SSI (Note: 2).
 
-  // g)Configure the MSTR bit (in multimaster NSS configuration, avoid conflict state on NSS if master is configured to prevent MODF error).
+  // g) Configure the MSTR bit (in multimaster NSS configuration, avoid conflict state on NSS if master is configured to prevent MODF error).
 
-  // h)Set the DFF bit to configure the data frame format (8 or 16 bits). Write to SPI_CR2 register:
+  // h) Set the DFF bit to configure the data frame format (8 or 16 bits).
 
-  //    a)Configure SSOE (Note: 1 & 2).
+  // 3:Write to SPI_CR2 register:
+  //    a) Configure SSOE (Note: 1 & 2).
 
-  //    b)Set the FRF bit if the TI protocol is required.
+  //    b) Set the FRF bit if the TI protocol is required.
 
   // 4.Write to SPI_CRCPR register: Configure the CRC polynomial if needed.
 
